c7e584338721f1742400a8ea934333f7f48f687d
al <apizano@purdue.edu>
2020-07-19 11:11:31 -0500

    Creating generic class for AggregatorProcessor

--- stock-analysis-generics/src/aggregators/AggregatorProcessor.java
+++ stock-analysis-generics/src/aggregators/AggregatorProcessor.java
@@ -1,6 +1,17 @@
 package aggregators;
 
-public class AggregatorProcessor {
-	
+public class AggregatorProcessor<E extends Aggregator> {
+    E agg;
+
+    public AggregatorProcessor(E agg, String filename) {
+        this.agg = agg;
+    }
+
+	public E runAggregator(int col) {
+        // get column
+        // get column values
+        agg.
+    }
+
 	
 }
